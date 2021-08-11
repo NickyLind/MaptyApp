4f2ef02adabdb68a64ebb582b5140a7a2a1ac62f
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-11 13:50:28 -0700

    add event listener that opens the workout form when clicking a spot on the map and then places the popup marker once the form is submitted

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if(navigator.geolocation) {
 
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -18,25 +20,44 @@ if(navigator.geolocation) {
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
     
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
-    map.on('click', function(mapEvent) {
-      const { lng, lat } = mapEvent.latlng
-      L.marker([lat, lng]).addTo(map)
-      .bindPopup(L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup'
-      }))
-      .setPopupContent('Placeholder Text')
-      .openPopup();
-    });
+    map.on('click', function(mapClickEvent) {
+      mapEvent = mapClickEvent;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    })
   }, function() {
     alert('Could not retrieve position')
   });
-}
+};
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Clear Input Fields
+  inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
+
+  //Display Markers
+  const { lng, lat } = mapEvent.latlng
+  L.marker([lat, lng]).addTo(map)
+  .bindPopup(L.popup({
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup'
+  }))
+  .setPopupContent('Placeholder Text')
+  .openPopup();
+  form.classList.add('hidden')
+});
+
+inputType.addEventListener('change', function() {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
